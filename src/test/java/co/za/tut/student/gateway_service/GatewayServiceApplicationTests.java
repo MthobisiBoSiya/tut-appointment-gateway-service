57dbd67616fd1578bcc39aabe14cf517d3e18e7b
scongo <khozavictor426@gmail.com>
2026-05-10 20:23:45 +0200

    initial commit

---
+++ src/test/java/co/za/tut/student/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.tut.student.gateway_service;
+
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
